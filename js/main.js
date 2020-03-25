0237af7994e7a3e3e45e1c1f111a04e62fde4bb3
CarmineVitale <carmine.vitale90@gmail.com>
2020-03-25 16:19:50 +0100

    Lavoro completo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 
+// 1 chiedere all’utente il suo nome
+
+var nome = prompt('Qual è il tuo nome?')
+console.log(nome);
+
+
+
+// 2 chiedere il cognome
+
+var cognome = prompt('Qual è il tuo cognome?')
+console.log(cognome);
+
+
+
+
+// 3 chiedere il colore preferito
+
+var colore = prompt('Qual è il tuo colore preferito?')
+console.log(colore);
+
+
+
+
+
+// 4 scrivere nella pagina le informazioni ottenuti dall’utente, nel formato: nomecognomecolorepreferito20
+var password = nome + cognome + colore + '20';
+console.log(password);
+document.getElementById('password').innerHTML = password;
